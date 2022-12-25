984df0d5901bfbeb3af981b9c38a07bad82ffe1f
NidokinG22 <117809138+NidokinG22@users.noreply.github.com>
2022-12-25 23:14:45 +0300

    Update lab1dz.py

--- lab1dz/lab1dz.py
+++ lab1dz/lab1dz.py
@@ -1,7 +1,11 @@
+import sys
 a = int(input())
 b = int(input())
 c = int(input())
 D = (b ** 2) - (4 * a * c)
+if a==0:
+    print("Уравнение не квадратное")
+    sys.exit(0)
 if D >= 0:
     x1 = (-b + D ** 0.5) / (2 * a)
     x2 = (-b - D ** 0.5) / (2 * a)
